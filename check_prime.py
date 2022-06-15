3b64659fbcc808c431c0997623a5bb998c907462
Rosa Donat <rosalinda.donat@gmail.com>
2022-06-15 16:56:18 -0400

    Python challenge

---
+++ check_prime.py
@@ -0,0 +1,35 @@
+def check_prime(number):
+
+    """
+
+    Prints out if the number is prime or not
+
+    """
+
+    flag = False
+
+    if number == 0 or number == 1 or number < 0:
+
+        print(f"{number} is not prime number")
+
+    else:
+
+        for check_number in range(2, 1+number//2):
+
+            if number % check_number == 0:
+
+                print(f"{number} is not prime number")
+                flag = True
+
+                break
+
+ 
+
+        if not flag:
+
+            print(f"{number} is prime number")
+			
+			
+		
+check_prime(17)
+
